5fa21db8497dd0026a5aee42678fc2c6d564af06
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-09-26 11:00:31 +0530

    bug fix

--- src/io/events/roomClientEvents.js
+++ src/io/events/roomClientEvents.js
@@ -1,25 +1,46 @@
+const getWhiteSpace = (word, max = 0) =>{
+    let length = max - word.length;
+    for (let x=0; x<length; x++) {
+        word += " ";
+    }
+    return word;
+}
+const debug = (incoming=true, event, from,  to="", room="") =>{
 
+    room = getWhiteSpace(room, 15);
+    event = getWhiteSpace(event, 10);
+
+    let emoji = '\x1b[32m'+incoming?"↓":"↑" +'\x1b[0m';
+
+    console.log(
+        '\x1b[36m', `conference-server:socket`,'\x1b[0m',
+        '\x1b[32m', incoming?"↓":"↑" ,'\x1b[0m',
+        `\x1b[35m${room}\x1b[0m`,
+        event,
+        `\x1b[35m${from}\x1b[0m`,
+        '\x1b[32m', "→" ,'\x1b[0m',
+        `\x1b[35m${to}\x1b[0m`)
+
+}
 module.exports =  (io, socket) =>{
     const room = socket.handshake.query.room_id;
     const name = socket.handshake.query.name;
-    let debug = require('debug')(`conference-server:socket:${room}`);
-
     socket.on("join", ()=>{
-        debug(socket.id ," has joined to ===> ", room);
+        debug(true , "JOIN", socket.id,"Broker",room);
         socket.broadcast.to(room).emit("join", {
             id: socket.id,
             name: name
         });
     });
     socket.on("candidate", (event) => {
-        debug("candidate received from ===> ", socket.id, "for===>", event.to);
+        debug(true , "CANDIDATE", socket.id, event.to, room);
         socket.to(event.to).emit("candidate", {
             from: socket.id,
             candidate:event.candidate
         });
     });
     socket.on("offer", (event) => {
-        debug("offer received from ===> ", socket.id, "for===>", event.to);
+        debug(true , "OFFER", socket.id, event.to, room);
         socket.to(event.to).emit("offer", {
             from: socket.id,
             name: name,
@@ -27,7 +48,7 @@ module.exports =  (io, socket) =>{
         });
     });
     socket.on('answer', (event) => {
-        debug("answer received from ===> ", socket.id, "for===>", event.to);
+        debug(true , "ANSWER", socket.id, event.to, room);
         socket.to(event.to).emit('answer', {
             from : socket.id,
             answer: event.answer
@@ -35,7 +56,7 @@ module.exports =  (io, socket) =>{
     })
 
     socket.on("disconnect", () => {
-        debug("disconnected from ===> ", socket.id);
+        debug(false , "DISCONNECT", socket.id, "Everyone", room);
         socket.broadcast.to(room).emit("detached", socket.id);
         socket.leave(room);
     });
